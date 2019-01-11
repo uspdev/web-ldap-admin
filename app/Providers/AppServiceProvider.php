77b2cf2d2b62a8202c5a391884de0009b4488ad1
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2019-01-11 17:06:15 -0200

    forçar https em produção

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,10 +18,10 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
 
         // Forçar https em produção
-        /*
+        
         if (env('APP_ENV') === 'production') {
             \URL::forceScheme('https');
-        } */
+        } 
     }
 
     /**
