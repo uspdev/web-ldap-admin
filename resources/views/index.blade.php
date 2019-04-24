24bccaff15c9185eb5f4d34eba453f83f49679ee
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2019-04-24 15:19:30 -0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 @extends('adminlte::page')
 
-@section('title', 'Senha FLFCH')
+@section('title', 'USP')
 
 @section('content_header')
     <h1></h1>
@@ -10,7 +10,7 @@
 @include('alerts')
     @auth
         <h3><b>Olá {{ Auth::user()->name }},</b></h3>
-        Acesse sua <a href="/ldapusers/my"> área restrita </a> para definir ou alterar a <i>senha FFLCH</i>
+        Acesse a <a href="/ldapusers/my"> área restrita </a> para definir ou alterar sua <i>senha</i>
     @else
         Você ainda não fez seu login com a senha única USP <a href="/login"> Faça seu Login! </a>
     @endauth
