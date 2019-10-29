087e71fd377c8f435c05fcd2857fe69af4ee7396
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2019-10-29 10:05:18 -0300

    Pequena mudança para se alterar o footer

--- config/web-ldap-admin.php
+++ config/web-ldap-admin.php
@@ -1,16 +1,17 @@
 <?php
 
 return [
-
-
-    'senha_unica_key' => env('SENHAUNICA_KEY', false),
-    'senha_unica_secret' => env('SENHAUNICA_SECRET', false),
-    'senha_unica_callback_id' => env('SENHAUNICA_CALLBACK_ID', false),
+    # Footer theme
+    'footer' => 'ECA', # Talvez trazer a sigla da unidade a partir do codund via replicado
+    
+    # Senha única
+    'senha_unica_key'           => env('SENHAUNICA_KEY', false),
+    'senha_unica_secret'        => env('SENHAUNICA_SECRET', false),
+    'senha_unica_callback_id'   => env('SENHAUNICA_CALLBACK_ID', false),
 
     # Unidades autorizadas
     'replicado_unidade' => env('REPLICADO_UNIDADE'),
 
     # Admins
     'admins' => env('ADMINS'),
-
 ];
