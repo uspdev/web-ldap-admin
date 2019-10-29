856ef3885b256904d08517cd4a7feb11557c4f83
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2019-10-29 13:44:34 -0300

    Adldap2-laravel v.6
    Adicionadas as configurações para adldap2/adldap2-laravel v.6
    no .env.example e também no config/ldap.php

---
+++ resources/views/vendor/laravel-usp-theme/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<!-- Footer -->
+<footer>
+    <div class="footer-copyright text-center py-4">
+      @section('footer')
+        <a href="https://github.com/uspdev/">© 2019 USPDev</a>
+      @show
+    </div>
+</footer>
