fda6106ace33f17bf1815bc055173a38b926b513
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2019-10-29 14:04:01 -0300

    Removidos arquivos de views criados no publish
    Executar somente os comandos:
    php artisan vendor:publish --provider="Adldap\Laravel\AdldapAuthServiceProvider" --tag=assets --force
    php artisan vendor:publish --provider="Adldap\Laravel\AdldapServiceProvider" --tag=assets --force

--- resources/views/vendor/laravel-usp-theme/partials/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<!-- Footer -->
-<footer>
-    <div class="footer-copyright text-center py-4">
-      @section('footer')
-        <a href="https://github.com/uspdev/">© 2019 USPDev</a>
-      @show
-    </div>
-</footer>
