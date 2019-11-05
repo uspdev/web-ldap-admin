e97c97733c7712f32db3d78859e63085547b64b7
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2019-11-05 18:06:31 -0300

    Alterada a view para se escolher mais vínculos
    Mudei o layout para uma tabela a fim de se exibir a quantidade de contas
    a serem criadas a partir do Replicado e quantidade de contas já criadas
    no AD

--- resources/views/ldapusers/sync.blade.php
+++ resources/views/ldapusers/sync.blade.php
@@ -12,9 +12,71 @@
         <div class="col-md-6">
             <form method="post" action="/ldapusers/sync">
                 {{ csrf_field() }}
-                <input type="checkbox" name="type[]" value="servidores"> Funcionários<br>
-                <input type="checkbox" name="type[]" value="docentes"> Docentes<br>
-                <input type="checkbox" name="type[]" value="estagiarios"> Estagiários<br>            
+                <table class="table">
+                    <tr>
+                        <th>&nbsp;</th>
+                        <th>Vínculo</th>
+                        <th>Replicado</th>
+                        <th>&nbsp;</th>
+                        <th>AD</th>
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox" name="type[]" value="servidores"></td>
+                        <td>Funcionários</td>
+                        <td style="text-align: right;">999</td>
+                        <td>>></td>
+                        <td style="text-align: right;">999</td>
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox" name="type[]" value="docentes"></td>
+                        <td>Docentes</td>
+                        <td style="text-align: right;">999</td>
+                        <td>>></td>
+                        <td style="text-align: right;">999</td>
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox" name="type[]" value="estagiarios"></td>
+                        <td>Estagiários</td>
+                        <td style="text-align: right;">999</td>
+                        <td>>></td>
+                        <td style="text-align: right;">999</td>
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox" name="type[]" value="graducao"></td>
+                        <td>Alunos de Graduação</td>
+                        <td style="text-align: right;">999</td>
+                        <td>>></td>
+                        <td style="text-align: right;">999</td>
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox" name="type[]" value="posGraduacao"></td>
+                        <td>Alunos de Pós-Graduação</td>
+                        <td style="text-align: right;">999</td>
+                        <td>>></td>
+                        <td style="text-align: right;">999</td>
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox" name="type[]" value="posDoutorando"></td>
+                        <td>Alunos de Pós-doutorando</td>
+                        <td style="text-align: right;">999</td>
+                        <td>>></td>
+                        <td style="text-align: right;">999</td>
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox" name="type[]" value="culturaExtensao"></td>
+                        <td>Alunos de Cultura e Extensão</td>
+                        <td style="text-align: right;">999</td>
+                        <td>>></td>
+                        <td style="text-align: right;">999</td>
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox" name="type[]" value="escolaArteDramatica"></td>
+                        <td>Alunos da Escola de Arte Dramática</td>
+                        <td style="text-align: right;">999</td>
+                        <td>>></td>
+                        <td style="text-align: right;">999</td>
+                    </tr>                    
+                </table>
 
                 <div class="form-group">
                   <input type="submit" class="btn btn-primary" value="Sincronizar">
