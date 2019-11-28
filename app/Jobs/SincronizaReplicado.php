cb302d5f9160aa1015bd9ccd3d5a8badd8916e28
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2019-11-28 15:58:26 -0300

    Separadas as contas de quem desligou em um array desligados

--- app/Jobs/SincronizaReplicado.php
+++ app/Jobs/SincronizaReplicado.php
@@ -52,6 +52,47 @@ class SincronizaReplicado implements ShouldQueue
     public function sync($pessoas)
     {
         if ($pessoas) {
+            
+            // No .env foi configurado para desativar os desligados?
+            if (config('web-ldap-admin.desativarDesligados') == true) {
+                # Situações:
+                # 1) Perdeu vínculo com a unidade, remover dos grupos, adicionar ao grupo Desativados e destivar a conta. 
+                # 2) Não perdeu vínculo, mas trocou de setor, efetuar a troca de grupo.
+                
+                /**
+                 * Para se verificar os desligados
+                 * Comparar as contas do AD por grupo principal
+                 * Servidor, Docente, Estagiário,
+                 * Aluno de Graduação, Aluno de Pós-Graduação, Aluno de Cultura e Extensão, 
+                 * Aluno Escola de Arte Dramática, Pós-doutorando
+                 */   
+
+                // Identifica o grupo principal
+                $grupoPrincipal = $pessoas[0]['tipvinext'];
+                // Array das pessoas do replicado
+                $replicadoUsers = [];
+                foreach ($pessoas as $pessoa) {
+                    array_push($replicadoUsers, $pessoa['codpes']);
+                }
+                // Array das contas no AD
+                $group = Adldap::search()->groups()->find($grupoPrincipal);
+                $ldapusers = Adldap::search()->users();
+                $ldapusers = $ldapusers->where('memberof', '=', $group->getDnBuilder()->get());
+                $ldapusers = $ldapusers->where('samaccountname','!=','Administrator');
+                $ldapusers = $ldapusers->where('samaccountname','!=','krbtgt');
+                $ldapusers = $ldapusers->where('samaccountname','!=','Guest');
+                $ldapusers = $ldapusers->sortBy('displayname', 'asc');
+                $ldapusers = $ldapusers->paginate(config('web-ldap-admin.registrosPorPagina'))->getResults();
+                $contasAD = [];
+                foreach ($ldapusers as $ldapuser) {
+                    array_push($contasAD, $ldapuser->getAccountName());
+                }
+                // Verifica se alguma conta no AD não existe no replicado e guarda no array de desligados
+                $desligados = array_values(array_diff($contasAD, $replicadoUsers));
+                // Estas contas devem entram na situação 1 
+                // dd($desligados);
+            }
+
             foreach($pessoas as $pessoa) {
                 $vinculos = Pessoa::vinculosSiglas($pessoa['codpes'],$this->unidade);
                 $setores = Pessoa::setoresSiglas($pessoa['codpes'],$this->unidade);
