eb435e05998cbec048207142b7a4839386b031fd
Masaki Kawabata Neto <kawabata@usp.br>
2022-03-14 16:55:24 -0300

    typo

--- resources/views/ldapusers/create.blade.php
+++ resources/views/ldapusers/create.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.app')
 
 @section('content_header')
-    <h1>Cadastrar usuário</h1>
+  <h1>Cadastrar usuário</h1>
 @stop
 
 @section('content')
 
-<div class="row">
-        <div class="col-md-6">
-            <form method="post" action="{{ url('/ldapusers') }}">
-                {{ csrf_field() }}
-                @include('partials.ldapusers.create-form')
-            </form>
-        </div>
+  <div class="row">
+    <div class="col-md-6">
+      <form method="post" action="{{ url('/ldapusers') }}">
+        {{ csrf_field() }}
+        @include('ldapusers.partials.create-form')
+      </form>
     </div>
+  </div>
 
 @stop
