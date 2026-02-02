e81807c3bab6f7d83919482edb1422d2fce95bd4
Leandro Ramos <leandroramos@disroot.org>
2026-02-02 12:27:36 -0300

    corrige release.sh

--- dokku-deploy/release.sh
+++ dokku-deploy/release.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
+set -e
+
 mkdir -p storage/framework/cache storage/framework/sessions storage/framework/views storage/logs bootstrap/cache
+
 chown -R www-data:www-data storage bootstrap/cache
 chmod -R 775 storage bootstrap/cache
 
-composer install --no-dev --optimize-autoloader --no-interaction --no-scripts
+composer install --no-dev --optimize-autoloader --no-interaction
+
+php artisan config:clear
+php artisan cache:clear
+php artisan config:cache
+php artisan route:cache
+
 php artisan migrate --force
