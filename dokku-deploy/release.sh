51d5c6ab68161dec112f76823cfc7e9e76936c42
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2026-08-20 10:58:03 -0300

    removendo arquivos dokku não usados

--- dokku-deploy/release.sh
+++
@@ -1,9 +0,0 @@
-bash#!/bin/bash
-set -e
-
-mkdir -p storage/framework/cache storage/framework/sessions storage/framework/views storage/logs bootstrap/cache
-chown -R www-data:www-data storage bootstrap/cache
-chmod -R 775 storage bootstrap/cache
-composer install --no-dev --optimize-autoloader --no-interaction
-php artisan config:clear
-php artisan cache:clear
